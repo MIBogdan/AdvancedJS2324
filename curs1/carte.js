c3353df803de3f9358ffda657c7d2cac149d5552
xMariuss <mariuss.b094@gmail.com>
2024-04-04 20:51:05 +0300

    advancedjs2324

--- curs1/carte.js
+++ curs1/carte.js
@@ -10,7 +10,7 @@ class Carte {
         console.log('Am deschis cartea la pagina 15');
     }
     randomIntFromInterval(min, max) { // min and max included 
-       return Math.floor(Math.random() * (max - min + 1) + min)
+       return Math.floor(Math.random() * (max - min + 1) + min);
      }
      
 
