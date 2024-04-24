75574428428c47cf7832bc4de5a1015d296fda29
xMariuss <mariuss.b094@gmail.com>
2024-04-24 19:59:51 +0300

    advancedjs2324

---
+++ curs6/ex1_2.js
@@ -0,0 +1,5 @@
+const calc = require('./proiectdemo/calculator.js');
+
+console.log(calc.sum(3, 2));
+console.log(calc.diff(3, 2));
+console.log(calc.product(3, 2));
---
+++ curs6/proiectdemo/calculator.js
@@ -0,0 +1,15 @@
+function sum (a, b) {
+    return a + b;
+}
+
+function diff (a, b) {
+    return a-b;
+}
+
+function product (a, b) {
+    return a * b;
+}
+
+module.exports = {
+    sum, diff, product
+}
