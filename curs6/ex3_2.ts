cc214b1f03f15ce3450d1573602589a568481531
xMariuss <mariuss.b094@gmail.com>
2024-04-25 18:39:50 +0300

    advancedjs2324

--- curs6/ex3_2.ts
+++ curs6/ex3_2.ts
@@ -1,21 +1,23 @@
 class Student2 {  
     public studCode: number;  
-    protected studName: string;  
-    constructor(code: number, name: string){  
+    protected studName: string; 
+    private studYear: number; 
+    constructor(code: number, name: string, year: number){  
         this.studCode = code;  
         this.studName = name;  
+        this.studYear = year;
         }  
 }  
 class Person extends Student2 {  
     private department: string;  
   
-    constructor(code: number, name: string, department: string) {  
-        super(code, name);  
+    constructor(code: number, name: string, department: string, year: number) {  
+        super(code, name, year);  
         this.department = department;  
     }  
     public getElevatorPitch() {  
         return (`My unique code: ${this.studCode}, my name: ${this.studName} and I am in ${this.department} Branch.`);  
     }  
 }  
-let joeRoot: Person = new Person(1, "Ion", "PS");  
+let joeRoot: Person = new Person(1, "Ion", "PS", 2);  // dar year nu o sa mearga din cauza ca este private si poate fi folosit doar in clasa respectiva.
 console.log(joeRoot.getElevatorPitch());  
